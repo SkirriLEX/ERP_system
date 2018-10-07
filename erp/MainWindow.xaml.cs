4adf6d1f91cf137859aef42eed9d375f16c12a39
emets <emets@knu.ua>
2018-10-08 00:18:57 +0300

    Added SQL server ip address

--- erp/MainWindow.xaml.cs
+++ erp/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 
 namespace erp
 {
@@ -17,29 +14,8 @@ namespace erp
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            var dataQ = new DataQ();
-            try
-            {
-                Debug.WriteLine(@"Connecting to SQL Server ... ");
-                using (var connection = new SqlConnection(dataQ.Builder.ConnectionString))
-                {
-                    connection.Open();
-                    Debug.WriteLine(@"Done.");
-                }
-            }
-            catch (SqlException exception)
-            {
-                DataQ.DisplaySqlErrors(exception);
-                Debug.WriteLine(exception.ToString());
-            }
-            finally
-            {
-                using (var connection = new SqlConnection(dataQ.Builder.ConnectionString))
-                {
-                    connection.Close();
-                    Debug.WriteLine(@"Close.");
-                }
-            }
+            MessageBox.Show("1234567");
+            MessageBox.Show("yjrjhfjgjhb,jbk");
         }
     }
 }
