6ffc03c588727a5dadf4d922cd015464d8a308a1
kirril <kirk.andri@gmail.com>
2018-11-28 10:35:31 +0200

    Create style of tab plates, rebuild menu

--- erp/MainWindow.xaml.cs
+++ erp/MainWindow.xaml.cs
@@ -43,18 +43,23 @@ namespace erp
             Application.Current.Shutdown();
         }
 
-        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        private void PackIcon_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            ButtonOpenMenu.Visibility = Visibility.Collapsed;
-            ButtonCloseMenu.Visibility = Visibility.Visible;
-        }
-
-        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonOpenMenu.Visibility = Visibility.Visible;
-            ButtonCloseMenu.Visibility = Visibility.Collapsed;
 
         }
+
+        //private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        //    ButtonCloseMenu.Visibility = Visibility.Visible;
+        //}
+
+        //private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ButtonOpenMenu.Visibility = Visibility.Visible;
+        //    ButtonCloseMenu.Visibility = Visibility.Collapsed;
+
+        //}
     }
 
 }
