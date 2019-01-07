fdc28b600ca3f20b5218703657601515c469a65d
emets <emets@knu.ua>
2019-01-07 22:23:15 +0200

    fixed Speciality and Specialization classes

--- Test/DataQ.cs
+++ Test/DataQ.cs
@@ -2,16 +2,13 @@
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
 using System.Data;
-using System.Linq;
 
 namespace Test
 {
     public class DataQ
     {
         public readonly SqlConnectionStringBuilder Builder = new SqlConnectionStringBuilder();
-
         public DataQ()
         {
             const string connStr = "178.136.14.234";
@@ -20,7 +17,6 @@ namespace Test
             Builder.Password = "Resto#test01";
             //Builder.InitialCatalog = "WIN-NALRE9SA668\\SQLEXPRESS";
         }
-
         public SqlConnectionStringBuilder GetBuilder()
         {
             return Builder;
@@ -109,11 +105,9 @@ namespace Test
             return false;
         }
     }
-
     internal static class Utils
     {
         public static readonly DataQ Connect = new DataQ();
-
         public static int GetCountTuples(string tableName)
         {
             var count = 0;
@@ -296,140 +290,158 @@ namespace Test
             }
         }
     }
-        
 
-        public class Specialization //list
+    public class Specialization //list
+    {
+        private List<int> _specialityCode = new List<int>();
+        private List<int> _specializationCode = new List<int>();
+        private List<string> _nameSpecialization = new List<string>();
+
+        private Specialization()
         {
-            private List<int> _specialityCode;
-            private List<int> _specializationCode;
-            private List<string> _nameSpecialization;
-
-            private Specialization()
+            _specialityCode.Clear();
+            _specializationCode.Clear();
+            _nameSpecialization.Clear();
+        }
+        private Specialization(int specialityCode, int specializationCode, string nameSpecialization)
+        {
+            _specialityCode.Clear();
+            _specializationCode.Clear();
+            _nameSpecialization.Clear();
+            _specialityCode.Add(specialityCode);
+            _specializationCode.Add(specializationCode);
+            _nameSpecialization.Add(nameSpecialization);
+        }
+        public List<int> GetSpecialityCode()
+        {
+            return _specialityCode;
+        }
+        public List<int> GetSpecializationCode()
+        {
+            return _specializationCode;
+        }
+        public List<string> GetNameSpecialization()
+        {
+            return _nameSpecialization;
+        }
+        public void GetTableSpecialization()
+        {
+            _specialityCode.Clear();
+            _specializationCode.Clear();
+            _nameSpecialization.Clear();
+            var connection = new SqlConnection(Utils.Connect.Builder.ConnectionString);
+            try
             {
-                _specialityCode.Clear();
-                _specializationCode.Clear();
-                _nameSpecialization.Clear();
-            }
-
-            private Specialization(int specialityCode, int specializationCode, string nameSpecialization)
-            {
-                _specialityCode.Clear();
-                _specializationCode.Clear();
-                _nameSpecialization.Clear();
-                _specialityCode.Add(specialityCode);
-                _specializationCode.Add(specializationCode);
-                _nameSpecialization.Add(nameSpecialization);
-            }
-
-            public List<int> GetSpecialityCode()
-            {
-                return _specialityCode;
-            }
-
-            public List<int> GetSpecializationCode()
-            {
-                return _specializationCode;
-            }
-
-            public List<string> GetNameSpecialization()
-            {
-                return _nameSpecialization;
-            }
-
-            public void GetTableSpecialization()
-            {
-                _specialityCode.Clear();
-                _specializationCode.Clear();
-                _nameSpecialization.Clear();
-                var connection = new SqlConnection(Utils.Connect.Builder.ConnectionString);
-                try
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Specialization", connection);
+                using (var reader = command.ExecuteReader())
                 {
-                    connection.Open();
-                    var command = new SqlCommand("SELECT * FROM Specialization", connection);
-                    using (var reader = command.ExecuteReader())
+                    // while there is another record present
+                    while (reader.Read())
                     {
-                        // while there is another record present
-                        while (reader.Read())
-                        {
-                            // write the data on to the screen
-                            _specialityCode.Add(Convert.ToInt32(reader[0]));
-                            _specializationCode.Add(Convert.ToInt32(reader[1]));
-                            _nameSpecialization.Add(Convert.ToString(reader[2]));
-                            Debug.WriteLine($"{_specialityCode} \t | {_specializationCode} \t | {_nameSpecialization}");
-                        }
+                        // write the data on to the screen
+                        _specialityCode.Add(Convert.ToInt32(reader[0]));
+                        _specializationCode.Add(Convert.ToInt32(reader[1]));
+                        _nameSpecialization.Add(reader[2].ToString());
+                        Console.WriteLine($"{reader[0]} \t | {reader[1]} \t | {reader[2]}");
                     }
                 }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.ToString());
-                }
-                finally
-                {
-                    connection.Close();
-                }
             }
-
-            public void InsertToTableSpecialization(int specialityCode, int specializationCode, int nameSpecialization)
+            catch (Exception ex)
             {
-                var connection = new SqlConnection(Utils.Connect.Builder.ConnectionString);
-                try
-                {
-                    connection.Open();
-                    var command = new SqlCommand("INSERT INTO Specialization" +
-                                                 "(specialityCode, specializationCode, nameSpecialization)" +
-                                                 $"VALUES ({specialityCode}, " +
-                                                 $"{specializationCode}, " +
-                                                 $"{nameSpecialization})", connection);
-                    command.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.ToString());
-                }
-                finally
-                {
-                    connection.Close();
-                }
+                Console.WriteLine(ex.ToString());
             }
-
-            public void SearchInTableSpecialization(string arg)
+            finally
             {
-                _specialityCode.Clear();
-                _specializationCode.Clear();
-                _nameSpecialization.Clear();
-                var connection = new SqlConnection(Utils.Connect.Builder.ConnectionString);
-                try
+                connection.Close();
+            }
+        }
+        public void InsertToTableSpecialization(int specialityCode, int specializationCode, int nameSpecialization)
+        {
+            _nameSpecialization.Clear();
+            _specializationCode.Clear();
+            _specialityCode.Clear();
+            var connection = new SqlConnection(Utils.Connect.Builder.ConnectionString);
+            using (connection)
+            {
+                using (var command = new SqlCommand())
                 {
-                    connection.Open();
-                    var command = new SqlCommand("SELECT * FROM Specialization " +
-                                                 $"where specialityCode like {arg} or " +
-                                                 $"specializationCode like {arg} or" +
-                                                 $"nameSpecialization like {arg}", connection);
-                    using (var reader = command.ExecuteReader())
+                    command.Connection = connection; // <== lacking
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO Specialization (specialityCode, specializationCode, nameSpecialization) " +
+                                          "values (@specialityCode, @specializationCode, @nameSpecialization)";
+                    command.Parameters.AddWithValue("@specialityCode", specialityCode);
+                    command.Parameters.AddWithValue("@specializationCode", specializationCode);
+                    command.Parameters.AddWithValue("@nameSpecialization", nameSpecialization);
+
+                    try
                     {
-                        // while there is another record present
-                        while (reader.Read())
+                        connection.Open();
+                        var recordsAffected = command.ExecuteNonQuery();
+                        using (var reader = command.ExecuteReader())
                         {
-                            // write the data on to the screen
-                            _specialityCode.Add(Convert.ToInt32(reader[0]));
-                            _specializationCode.Add(Convert.ToInt32(reader[1]));
-                            _nameSpecialization.Add(Convert.ToString(reader[2]));
-                            Debug.WriteLine($"{_specialityCode} \t | {_specializationCode} \t | {_nameSpecialization}");
+                            // while there is another record present
+                            while (reader.Read())
+                            {
+                                // write the data on to the screen
+                                _specialityCode.Add(Convert.ToInt32(reader[0]));
+                                _specializationCode.Add(Convert.ToInt32(reader[1]));
+                                _nameSpecialization.Add(reader[2].ToString());
+                                Console.WriteLine($"{reader[0]} \t | {reader[1]} \t | {reader[2]}");
+                            }
                         }
                     }
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.ToString());
-                }
-                finally
-                {
-                    connection.Close();
+                    catch (SqlException ex)
+                    {
+                        DataQ.DisplaySqlErrors(ex);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
                 }
             }
         }
+        public void SearchInTableSpecialization(string arg)
+        {
+            GetTableSpecialization();
+            var index = new List<int>();
+            if (int.TryParse(arg, out var code))
+            {
+                for (var i = 0; i < _specialityCode.Count; i++)
+                    if (_specialityCode[i] == code || _specializationCode[i] == code) index.Add(i);
+            }
+            else
+            {
+                for (var i = 0; i < _specialityCode.Count; i++)
+                    if (_nameSpecialization[i]==arg) index.Add(i);
+            }
 
-        public class InfLogin //list
+            var specialityCodeTmp = new List<int>();
+            var specializationCodeTmp = new List<int>();
+            var nameTmp = new List<string>();
+
+            foreach (var t in index)
+            {
+                nameTmp.Add(_nameSpecialization[t]);
+                specialityCodeTmp.Add(_specialityCode[t]);
+                specializationCodeTmp.Add(_specializationCode[t]);
+            }
+            _specialityCode.Clear();
+            _specialityCode = specialityCodeTmp;
+            _specializationCode.Clear();
+            _specializationCode = specializationCodeTmp;
+            _nameSpecialization.Clear();
+            _nameSpecialization = nameTmp;
+
+            for (var i = 0; i < _specialityCode.Count; i++)
+            {
+                Console.WriteLine($"{_specialityCode} \t | {_specializationCode} \t | {_nameSpecialization}");
+            }
+        }
+    }
+
+    public class InfLogin //list
         {
             private List<int> _tabNumPerson;
             private List<string> _loginStr;
